2c56d16773305a1a34df5aa42ca136603b41fcb9
Roko <rokokovac98@gmail.com>
2023-10-23 21:40:35 +0200

    Set KeepAliveTimerInMiliseconds to 5000

--- Chat.Api/LongPollingConnectionStatusChecker.cs
+++ Chat.Api/LongPollingConnectionStatusChecker.cs
@@ -2,12 +2,10 @@
 
 // As we cannot rely on the client to close the connection gracefully,
 // We need to do a manual check to see if the user is still connected.
-// This scales terribly, but the alternative is 
 // We consider a lack of long poll for longer than KeepAliveTimerInMiliseconds to be a disconnect.
-public class LongPollingConnectionStatusChecker(LongPollingUserRepository repo, MessagingService service) : BackgroundService
+public class LongPollingConnectionStatusChecker(LongPollingUserRepository repo, MessagingService service, ILogger<LongPollingAdapter> logger) : BackgroundService
 {
-    private const int KeepAliveTimerInMiliseconds = 500;
-    private const int CheckIntervalInMiliseconds = 1000;
+    private const int KeepAliveTimerInMiliseconds = 5000;
 
     protected override Task ExecuteAsync(CancellationToken ct) => Task.Run(async () =>
     {
@@ -17,12 +15,13 @@ public class LongPollingConnectionStatusChecker(LongPollingUserRepository repo, 
             {
                 if (DateTimeOffset.UtcNow - user.LastSeen > TimeSpan.FromMilliseconds(KeepAliveTimerInMiliseconds))
                 {
+                    logger.LogWarning("Disconnecting user {name} due to timeout", name);
                     await service.RemoveUser(name);
                     await user.Connection.CloseConnection("Timeout");
                 }
             }
             
-            await Task.Delay(CheckIntervalInMiliseconds, ct);
+            await Task.Delay(KeepAliveTimerInMiliseconds, ct);
         }
     }, ct);
 }
