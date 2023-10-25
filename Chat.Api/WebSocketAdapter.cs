ab779e811204136a363adabf5dda00aa3334aa19
Roko <rokokovac98@gmail.com>
2023-10-25 19:41:25 +0200

    Refactor WebSocketAdapter

--- Chat.Api/WebSocketAdapter.cs
+++ Chat.Api/WebSocketAdapter.cs
@@ -7,7 +7,14 @@ namespace Chat.Api;
 public class WebSocketAdapter(MessagingService messagingService, ILogger<WebSocketAdapter> logger) : IConnectionAdapter
 {
     private WebSocket Socket;
-
+    
+    public async Task SendMessage(Message message)
+    {
+        var messageString = JsonSerializer.Serialize(message);
+        var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(messageString));
+        await Socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+    }
+    
     public async Task CloseConnection(string reason)
     {
         await Socket.CloseAsync(WebSocketCloseStatus.PolicyViolation, reason, default);
@@ -16,32 +23,18 @@ public class WebSocketAdapter(MessagingService messagingService, ILogger<WebSock
     public async Task HandleUser(HttpContext context, string name)
     {
         Socket = await context.WebSockets.AcceptWebSocketAsync();
-
-        var error = await messagingService.TryAddUser(this, name);
-        if (error != null)
+        
+        var addUserError = await messagingService.TryAddUser(this, name);
+        if (addUserError != null)
         {
-            await Socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, error, default);
+            await Socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, addUserError, default);
             return;
         }
 
         try
         {
-            await Receive(Socket, name, async (result, buffer) =>
+            await Receive(Socket, async (messageString) =>
             {
-                if (result.MessageType == WebSocketMessageType.Close)
-                {
-                    await messagingService.RemoveUser(name);
-                    await Socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, default);
-                    return;
-                }
-                
-                if (result.MessageType != WebSocketMessageType.Text)
-                {
-                    await Socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "Expected text message", default);
-                    return;
-                }
-                
-                var messageString = Encoding.UTF8.GetString(buffer);
                 var message = JsonSerializer.Deserialize<ChatMessage>(messageString);
                 if (message == null)
                 { 
@@ -51,9 +44,10 @@ public class WebSocketAdapter(MessagingService messagingService, ILogger<WebSock
                     return;
                 }
                         
-                var chatMessage = JsonSerializer.Deserialize<ChatMessage>(messageString);
-                await messagingService.BroadcastMessage(chatMessage);
+                await messagingService.BroadcastMessage(message);
             });
+            
+            await messagingService.RemoveUser(name);
         }
         catch (WebSocketException e)
         {
@@ -62,8 +56,7 @@ public class WebSocketAdapter(MessagingService messagingService, ILogger<WebSock
         }
     }
 
-    private async Task Receive(WebSocket socket, string name,
-        Action<WebSocketReceiveResult, ArraySegment<byte>> handleMessage)
+    private async Task Receive(WebSocket socket, Func<string, Task> handleMessage)
     {
         var buffer = new byte[1024 * 2];
         while (socket.State == WebSocketState.Open)
@@ -71,26 +64,22 @@ public class WebSocketAdapter(MessagingService messagingService, ILogger<WebSock
             var result = await socket.ReceiveAsync(new ArraySegment<byte>(buffer), default);
             if (result.EndOfMessage == false)
             {
-                await messagingService.RemoveUser(name);
                 await socket.CloseAsync(WebSocketCloseStatus.MessageTooBig, "Max message size is 2KiB.", default);
                 return;
             }
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await Socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, default);
+                return;
+            }
+            if (result.MessageType != WebSocketMessageType.Text)
+            {
+                await Socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "Expected text message", default);
+                return;
+            }
+            var messageString = Encoding.UTF8.GetString(buffer[..result.Count]);
 
-            handleMessage(result, new ArraySegment<byte>(buffer, 0, result.Count));
+            await handleMessage(messageString);
         }
-        
-        // await messagingService.RemoveUser(name);
-    }
-
-    public async Task SendMessage(Message message)
-    {
-        // if (Socket.State != WebSocketState.Open)
-        // {
-        //     return;
-        // }
-        
-        var messageString = JsonSerializer.Serialize(message);
-        var buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(messageString));
-        await Socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
     }
 }
