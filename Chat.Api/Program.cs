f05f440fa81a3d4d2f4f7330e4dd418f8b335259
Roko <rokokovac98@gmail.com>
2023-10-26 19:17:13 +0200

    Add WIP WebTransport adapter
    The WebTransport feature doesn't seem to be working in the current version of Kestrel. This should be revisited once WebTransport is properly supported.

--- Chat.Api/Program.cs
+++ Chat.Api/Program.cs
@@ -1,6 +1,17 @@
 using Chat.Api;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel((_, options) =>
+{
+    // Port configured for WebTransport
+    options.ListenAnyIP(5003, listenOptions =>
+    {
+        listenOptions.UseHttps(Certificate.GenerateManualCertificate());
+        listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
+    });
+});
 builder.Services.AddLogging();
 builder.Services.AddSingleton<MessagingService>();
 builder.Services.AddTransient<WebSocketAdapter>();
@@ -20,8 +31,8 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 app.UseCors("AllowAnyOrigin");
 app.UseWebSockets();
-app.MapGet("/" , () => "WebSocket server");
-app.Map("/ws", async (HttpContext context, string name, WebSocketAdapter ws) =>
+
+app.Map("/ws", async (string name, HttpContext context, WebSocketAdapter ws) =>
 {
     if (context.WebSockets.IsWebSocketRequest)
     {
@@ -34,17 +45,52 @@ app.Map("/ws", async (HttpContext context, string name, WebSocketAdapter ws) =>
     }
 });
 
-app.MapGet("/lp", async (HttpContext context, CancellationToken ct, LongPollingAdapter service, string name, string? id) =>
-    await service.HandleLongPollingRequest(context, ct, name, id));
+app.Map("/wt", async (string name, HttpContext context, CancellationToken ct, WebTransportAdapter adapter) =>
+{
+    var feature = context.Features.GetRequiredFeature<IHttpWebTransportFeature>();
+    if (feature.IsWebTransportRequest)
+    {
+        var session = await feature.AcceptAsync(ct);
+        await adapter.HandleWebTransportRequest(session, name);
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync("Expected a WebSocket request");
+    }
+});
+
+app.Map("sse", async (HttpContext context, CancellationToken ct, ServerSentEventsAdapter adapter, string name) =>
+    await adapter.HandleServerSentEventsRequest(context, ct, name));
+
+app.MapGet("/lp", async (HttpContext context, CancellationToken ct, LongPollingAdapter adapter, string name, string? id) =>
+    await adapter.HandleLongPollingRequest(context, ct, name, id));
 
 app.MapPost("lp/message", async (MessagingService service, ChatMessage message) =>
 {
     await service.BroadcastMessage(message);
-
     return Results.Ok();
 });
 
-app.Map("sse", async (HttpContext context, CancellationToken ct, ServerSentEventsAdapter service, string name) =>
-    await service.HandleServerSentEventsRequest(context, ct, name));
+app.Run(async (context) =>
+{
+    var feature = context.Features.GetRequiredFeature<IHttpWebTransportFeature>();
+    if (!feature.IsWebTransportRequest)
+    {
+        return;
+    }
+    var session = await feature.AcceptAsync(CancellationToken.None);
+
+    // open a new stream from the server to the client
+    var stream = await session.OpenUnidirectionalStreamAsync(CancellationToken.None);
+
+    if (stream is not null)
+    {
+        // write data to the stream
+        var outputPipe = stream.Transport.Output;
+        await outputPipe.WriteAsync(new Memory<byte>(new byte[] { 65, 66, 67, 68, 69 }), CancellationToken.None);
+        await outputPipe.FlushAsync(CancellationToken.None);
+    }
+});
 
 app.Run();
